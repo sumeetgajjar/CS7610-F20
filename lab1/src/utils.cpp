5df377aa2a925102fcd69e91472c2099914aa47e
gajjar.s <gajjar.s@husky.neu.edu>
2020-10-01 11:34:23 -0700

    Use VLOG to prevent log flooding

--- lab1/src/utils.cpp
+++ lab1/src/utils.cpp
@@ -15,18 +15,16 @@
 namespace lab1 {
 
     std::string Utils::getCurrentContainerHostname() {
-        LOG(INFO) << "getting current host";
+        VLOG(1) << "getting current host";
         char hostname[HOST_NAME_MAX];
-        int errorCode = gethostname(hostname, HOST_NAME_MAX);
-        if (errorCode) {
-            LOG(FATAL) << "cannot find the current host name";
-        }
+        int status = gethostname(hostname, HOST_NAME_MAX);
+        CHECK(status == 0) << "cannot find the current host name, errorCode: " << errno;
         LOG(INFO) << "current host name: " << hostname;
         return std::string(hostname);
     }
 
     std::vector<std::string> Utils::readHostFile(const std::string &hostFilePath) {
-        LOG(INFO) << "reading host file, path: " << hostFilePath;
+        VLOG(1) << "reading host file, path: " << hostFilePath;
         std::vector<std::string> hostnames;
         std::set<std::string> uniqueHostnames;
 
@@ -40,15 +38,13 @@ namespace lab1 {
         }
         hostFile.close();
         LOG(INFO) << hostnames.size() << " hosts found in hostfile";
-        if (hostnames.size() < 2) {
-            LOG(FATAL) << "hostfile should contain more than 1 hosts";
-        }
+        CHECK(hostnames.size() > 3) << "hostfile should contain more than 3 hosts";
         return hostnames;
     }
 
     std::vector<std::string> Utils::getPeerContainerHostnames(const std::vector<std::string> &allHostnames,
                                                               const std::string &currentHostname) {
-        LOG(INFO) << "getting peer container host names";
+        VLOG(1) << "getting peer container host names";
         std::vector<std::string> peerHostnames;
         for (const auto &hostname: allHostnames) {
             if (hostname != currentHostname) {
@@ -61,7 +57,7 @@ namespace lab1 {
 
     unsigned int Utils::getProcessIdentifier(const std::vector<std::string> &allHostnames,
                                              const std::string &hostname) {
-        LOG(INFO) << "getting process identifier";
+        VLOG(1) << "getting process identifier";
         for (unsigned int i = 0; i < allHostnames.size(); i++) {
             if (allHostnames[i] == hostname) {
                 unsigned int processIdentifier = i + 1;
