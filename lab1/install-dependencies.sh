47b430db74119aac278b1be35f4814f86b9d9b9d
gajjar.s <gajjar.s@husky.neu.edu>
2020-09-30 19:02:56 -0700

    Use find package for gflag and glog

--- lab1/install-dependencies.sh
+++ lab1/install-dependencies.sh
@@ -11,15 +11,15 @@ echo "Creating dependencies dir: '${DEPENDENCIES}'"
 mkdir ${DEPENDENCIES}
 
 conan install . \
-    --generator cmake \
-    --install-folder=${DEPENDENCIES} \
-    -r conan-center \
-    -s os=Linux \
-    -s os_build=Linux \
-    -s arch=x86_64 \
-    -s arch_build=x86_64 \
-    -s compiler=gcc \
-    -s compiler.version=7 \
-    -s compiler.libcxx=libstdc++11 \
-    -s build_type=Release \
-    -s cppstd=17
+  --generator cmake_find_package \
+  --install-folder=${DEPENDENCIES} \
+  -r conan-center \
+  -s os=Linux \
+  -s os_build=Linux \
+  -s arch=x86_64 \
+  -s arch_build=x86_64 \
+  -s compiler=gcc \
+  -s compiler.version=7 \
+  -s compiler.libcxx=libstdc++11 \
+  -s build_type=Release \
+  -s cppstd=17
